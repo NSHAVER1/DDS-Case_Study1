82b307445b0b100d7bc8a86d5ef69251369263a6
Nicole Shaver <nshaver@smu.edu>
2017-03-02 23:05:05 -0700

    update rmd file-change setup to check for packages and setwd there, add q1 and 2 code, update q3 answer in text and code to aggregate by group. dowload.R- remove package install since it is now in main .rmd

--- source/download.R
+++ source/download.R
@@ -1,12 +1,5 @@
-#libraries required to download the data
-#install.packages("downloader")
-library(downloader)
-
-#set the working directory
-setwd("C:\\Users\\nmsha\\Documents\\SMU\\DoingDataScience\\homework\\CaseStudy1")
-dir<-getwd()
-
 #download the 2 files
+dir<-getwd()
 downloader::download("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv", destfile=paste(dir,"/data/GDP.csv",sep=""))
 downloader::download("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv", destfile=paste(dir,"/data/EDSTATS.csv",sep=""))
 
