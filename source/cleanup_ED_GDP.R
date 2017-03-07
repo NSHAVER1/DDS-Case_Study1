1c1abba82f28f34b7b654efa68af5d849363c053
Nicole Shaver <nshaver@smu.edu>
2017-03-06 21:43:44 -0700

    fixed cleanup code to read 190 rows, fixed rmd to include ggplot and answer to Q1

--- source/cleanup_ED_GDP.R
+++ source/cleanup_ED_GDP.R
@@ -5,12 +5,15 @@ names(GDP2)<-c("CountryCode","GDP.ranking","GDP.Country","GDP")
 #delete the rows with Country Codes that are blank
 GDP2<-subset(GDP2, CountryCode!="")
 
+#remove all rows with GDP.ranking is blank so we get only the first 190 rows as specified in the instructions
+GDP2<-subset(GDP2,GDP2$GDP.ranking!="" )
 
 #make the rankings into numbers, not characters
 GDP2$GDP.ranking<-as.numeric(GDP2$GDP.ranking)
 
 #make the GDP column into a number, not characters by first replacing the commas then forcing it to be numeric
 GDP2$GDP<-as.numeric(gsub(",", "", GDP2$GDP))
+
 
 #count the number of NA in ranking and GDP
 sum(is.na(GDP2$GDP.ranking))
