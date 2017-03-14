4e609baf8e7ef3b5b820e9bf0e17087b3a316a75
Nathan Tuttle <nathan.tuttle@gmail.com>
2017-03-13 19:08:40 -0700

    Put download.R

--- source/download.R
+++ source/download.R
@@ -1,9 +1,9 @@
 #download the 2 files
 dir<-getwd()
-downloader::download("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv", destfile="../data/GDP.csv")
-downloader::download("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv", destfile="../data/EDSTATS.csv")
+downloader::download("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv", destfile=paste(dir,"/data/GDP.csv",sep=""))
+downloader::download("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv", destfile=paste(dir,"/data/EDSTATS.csv",sep=""))
 
 
 #assign the two files to dataframes-replace blanks with NA
-GDP<-read.csv("/data/GDP.csv",stringsAsFactors=FALSE, header=FALSE)
-EDDATA<-read.csv("/data/EDSTATS.csv",stringsAsFactors=FALSE, header=TRUE, na.strings=c(""))
+GDP<-read.csv("data/GDP.csv",stringsAsFactors=FALSE, header=FALSE)
+EDDATA<-read.csv("data/EDSTATS.csv",stringsAsFactors=FALSE, header=TRUE, na.strings=c(""))
