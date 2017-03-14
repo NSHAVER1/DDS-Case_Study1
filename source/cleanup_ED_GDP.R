aacd994284199ebd8bca29e6202dc59342927ab3
Nathan Tuttle <nathan.tuttle@gmail.com>
2017-03-13 18:54:59 -0700

    Put cleanup_ED_GDP.R back

--- source/cleanup_ED_GDP.R
+++ source/cleanup_ED_GDP.R
@@ -5,26 +5,19 @@ names(GDP2)<-c("CountryCode","GDP.ranking","GDP.Country","GDP")
 #delete the rows with Country Codes that are blank
 GDP2<-subset(GDP2, CountryCode!="")
 
+#remove all rows with GDP.ranking is blank so we get only the first 190 rows as specified in the instructions
+GDP2<-subset(GDP2,GDP2$GDP.ranking!="" )
 
 #make the rankings into numbers, not characters
 GDP2$GDP.ranking<-as.numeric(GDP2$GDP.ranking)
 
-#No Ranking can be quantified if it does not exist. Ranking will only be included for existing lines
-
 #make the GDP column into a number, not characters by first replacing the commas then forcing it to be numeric
 GDP2$GDP<-as.numeric(gsub(",", "", GDP2$GDP))
 
-combo<-merge(GDP2,EDDATA, by="CountryCode", all=TRUE, SORT=TRUE)
 
 #count the number of NA in ranking and GDP
 sum(is.na(GDP2$GDP.ranking))
 sum(is.na(GDP2$GDP))
 
-
-
-
-
-
-
-
-
+#clean up the EDDATA
+#no clean up needed for our analysis
